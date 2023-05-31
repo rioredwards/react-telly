d82a993808c1ac343cefb80e36b38d5c9d62772d
Molly Jean Bennett <molly.bennett@equipmentshare.com>
2023-05-31 16:53:44 -0700

    Lesson 1 demo

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { PageWrapper } from './home.styles'
 import { Header, GifViewer, Button } from '../../components'
 import { useGif } from '../../data/hooks/useGif'
 
 export const HomePage = () => {
-	//this is how you'll fetch data
 	const { data: gif, refetch: fetchNewGif, isLoading, isFetching, isError  } = useGif()
+	const [ isPlaying, setIsPlaying ] = useState<boolean>(false)
+	const buttonTitle: string = isPlaying ? 'Change the channel' : 'Start'
+
+	const onButtonClick = () => {
+		fetchNewGif()
+		if (!isPlaying){ 
+			setIsPlaying(true)
+		}
+	}
 
 	return (
 		<PageWrapper>
 			<Header>React Telly</Header>
-			<GifViewer />
+			<GifViewer gifUrl={gif?.data.images.downsized_medium.url} isError={isError} isLoading={isLoading || isFetching} />
+			<Button onClick={onButtonClick}>{ buttonTitle }</Button>
 		</PageWrapper>
 	)
 }
